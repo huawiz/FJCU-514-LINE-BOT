5de571f4ee066801ef1bf81d50dbc649c1aef594
Justin Wu <huawiz@icloud.com>
2024-05-21 17:05:24 +0800

    精簡app.py，並加入command.py處理指令

--- src/command.py
+++ src/command.py
@@ -11,36 +11,7 @@ linebotAPI = LineBotApi(os.getenv('Channel_Access_Token'))
 # Channel Secret
 webhookHandler = WebhookHandler(os.getenv('Channel_Secret_Token'))
 
-
-
-def handleNextClass(event):
-    try:
-        message = FlexSendMessage(LineMessage.msgNextClassText(), LineMessage.msgNextClass())
-        linebotAPI.reply_message(event.reply_token, message)
-    except Exception as e:
-        print(f"Error handling '下一節課': {e}")
-        message = TextSendMessage(text='資料異常，請嘗試更新課表')
-        linebotAPI.reply_message(event.reply_token, message)
-
-def handleUpdateCourse(event):
-    try:
-        scraper.updateCourse(os.getenv('StuAccount'), os.getenv('StuPassword'))
-        message = TextSendMessage(text='更新完成')
-        linebotAPI.reply_message(event.reply_token, message)
-    except Exception as e:
-        print(f"Error updating course: {e}")
-        message = TextSendMessage(text='更新異常')
-        linebotAPI.reply_message(event.reply_token, message)
-
-def handleCourseMenu(event):
-    message = FlexSendMessage('課表功能', LineMessage.msgCourseMenu())
-    linebotAPI.reply_message(event.reply_token, message)
-
-def handleDefault(event):
-    message = FlexSendMessage('?', LineMessage.defaultAction())
-    linebotAPI.reply_message(event.reply_token, message)
-
-@webhookHandler.add(MessageEvent, message=TextMessage)
+# 處理訊息
 def handleMessage(event):
     message = event.message.text
 
@@ -52,3 +23,37 @@ def handleMessage(event):
 
     webhookHandler = handlers.get(message, handleDefault)
     webhookHandler(event)
+
+# 指令:下一節課
+def handleNextClass(event):
+    try:
+        message = FlexSendMessage(LineMessage.msgNextClassText(), LineMessage.msgNextClass())
+        linebotAPI.reply_message(event.reply_token, message)
+    except Exception as e:
+        print(f"Error handling '下一節課': {e}")
+        message = TextSendMessage(text='資料異常，請嘗試更新課表')
+        linebotAPI.reply_message(event.reply_token, message)
+
+# 指令:更新課表
+def handleUpdateCourse(event):
+    try:
+        scraper.updateCourse(os.getenv('StuAccount'), os.getenv('StuPassword'))
+        message = TextSendMessage(text='更新完成')
+        linebotAPI.reply_message(event.reply_token, message)
+    except Exception as e:
+        print(f"Error updating course: {e}")
+        message = TextSendMessage(text='更新異常')
+        linebotAPI.reply_message(event.reply_token, message)
+
+# 指令:課表選單
+def handleCourseMenu(event):
+    message = FlexSendMessage('課表功能', LineMessage.msgCourseMenu())
+    linebotAPI.reply_message(event.reply_token, message)
+
+
+# 指令:預設(任何訊息)
+def handleDefault(event):
+    message = FlexSendMessage('?', LineMessage.defaultAction())
+    linebotAPI.reply_message(event.reply_token, message)
+
+
