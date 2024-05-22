ef57912289a1b08f1d474fae1a237f0c4c11e129
Justin Wu <huawiz@icloud.com>
2024-05-22 11:55:09 +0800

    修正部分函數

--- src/coursedata.py
+++ src/coursedata.py
@@ -8,70 +8,65 @@ from flask import render_template
 
 
 # 讀課表資料
-
 def getjsonCourseData():
-    file_path = os.path.join('data', 'courseData.json')
+    filePath = os.path.join('data', 'courseData.json')
     
     # 確保目錄存在
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    os.makedirs(os.path.dirname(filePath), exist_ok=True)
     
     # 嘗試打開並讀取檔案
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(filePath, 'r', encoding='utf-8') as f:
             jsonCourseData = json.load(f)
     except FileNotFoundError:
         # 如果檔案不存在，建立一個新的空檔案
         jsonCourseData = {}
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(filePath, 'w', encoding='utf-8') as f:
             json.dump(jsonCourseData, f, ensure_ascii=False, indent=4)
     
     return jsonCourseData
 
+#獲得今天的Weekday index
+def getIdxToday():
+    timezoneTW = pytz.timezone('Asia/Taipei')
+    return datetime.datetime.now(timezoneTW).weekday()
+
 #獲取今日課表資料
 def getTodayCourse(jsonCourseData):
-    timezoneTW = pytz.timezone('Asia/Taipei')
-    idxTodayWeekday = datetime.datetime.now(timezoneTW).weekday()
-    return getDayCourse(jsonCourseData,idxTodayWeekday)
+    return getDayCourse(jsonCourseData,getIdxToday())
 
 
 # 獲取指定日期課表資料
-def getDayCourse(jsonCourseData,intWeekDay):
-    
-    listDayCourse = []
-    for i in range(len(jsonCourseData)):
-        if jsonCourseData[i]['星期']==weekdayMapping[intWeekDay]:
-            listDayCourse.append(jsonCourseData[i])
-    if listDayCourse is not None:
-        return listDayCourse
-    else:
-        return None
+def getDayCourse(jsonCourseData, intWeekDay):
+    return [course for course in jsonCourseData if course['星期'] == weekdayMapping[intWeekDay]]
+
     
 #下一個有課日
 def getNextCourseDay():
     jsonCourseData = getjsonCourseData()
-    timezoneTW = pytz.timezone('Asia/Taipei')
-    idxToday = datetime.datetime.now(timezoneTW).weekday()
-    for i in range(idxToday+1,idxToday+8):
-        i = i%7
-        if getDayCourse(jsonCourseData,i):
-            return i
+    idxToday = getIdxToday()
+    for i in range(1, 8):
+        idx = (idxToday + i) % 7
+        if getDayCourse(jsonCourseData, idx):
+            return idx
     return None
 
-# 取得距離現在時間最接近的節次
-def getNextPeriodNearNow(strTimeCurrent):
-    strTimeCurrent = strTimeCurrent.strftime('%H:%M')  # 將現在時間轉換為字串格式，僅保留時分
-    dictSortedPeriods = sorted(dictMappingPeriodTime.items(), key=lambda x: x[1])  # 將節次開始時間排序
-    lenPeriods = len(dictSortedPeriods)
-    
-    for i in range(lenPeriods):
-        periodCode, periodTime = dictSortedPeriods[i]
-        nextPeriodCode, nextPeriodTime = dictSortedPeriods[(i + 1) % dictSortedPeriods]  # 循環遍歷節次列表
-        if periodTime > strTimeCurrent:
-            return periodCode, periodTime
-        elif nextPeriodTime <= strTimeCurrent < periodTime:  # 處理超過最後一個節次的情況
-            return nextPeriodCode, nextPeriodTime
-    return None, None  # 如果沒有找到下一個節次，則返回 None
 
+
+# 取得同天中，距離現在最近的下一堂課
+def getNextPeriodCourse(todayCourses, timeCurrent):
+    timeCurrentStr = timeCurrent.strftime('%H:%M')
+    nextCourse = None
+    minTimeDiff = float('inf')  # 初始設置一個極大值來存放最小時間差
+    for course in todayCourses:
+        courseTime = dictMappingPeriodTime.get(course['節次'].split('-')[0])  # 取得課程開始時間
+        if courseTime:
+            timeDiff = (datetime.datetime.strptime(courseTime, '%H:%M') - datetime.datetime.strptime(timeCurrentStr, '%H:%M')).total_seconds()
+            # 如果時間差為負，表示課程已經開始，不考慮這個課程
+            if timeDiff >= 0 and timeDiff < minTimeDiff:
+                minTimeDiff = timeDiff
+                nextCourse = course
+    return nextCourse
 
 # 取得下一堂課
 def getNextCourse(jsonCourseData, timeCurrent):
@@ -92,23 +87,8 @@ def getNextCourse(jsonCourseData, timeCurrent):
         if len(nextDayCoureSchedule):
             return nextDayCoureSchedule[0]
     
-    # 如果明天也沒有課程，返回 None
+    # 如果還是找不到
     return None
-
-# 取得同天中，距離現在最近的下一堂課
-def getNextPeriodCourse(todayCourses, timeCurrent):
-    timeCurrentStr = timeCurrent.strftime('%H:%M')
-    nextCourse = None
-    minTimeDiff = float('inf')  # 初始設置一個極大值來存放最小時間差
-    for course in todayCourses:
-        courseTime = dictMappingPeriodTime.get(course['節次'].split('-')[0])  # 取得課程開始時間
-        if courseTime:
-            timeDiff = (datetime.datetime.strptime(courseTime, '%H:%M') - datetime.datetime.strptime(timeCurrentStr, '%H:%M')).total_seconds()
-            # 如果時間差為負，表示課程已經開始，不考慮這個課程
-            if timeDiff >= 0 and timeDiff < minTimeDiff:
-                minTimeDiff = timeDiff
-                nextCourse = course
-    return nextCourse
 
 
 # WebView使用
@@ -180,11 +160,10 @@ def mergeSameRowHtml(htmlContent):
     modifiedHtml = soup.prettify()
     return modifiedHtml
 
+# 渲染課表HTML
 def renderCourse():
-    courseName = '課表'
-    scheduleData=getScheduleData()
-    periodMapping = {'D0': 0,'D1': 1,'D2': 2,'D3': 3,'D4': 4,'DN': 5,'D5': 6,'D6': 7,'D7': 8,'D8': 9,'E0': 10,'E1': 11,'E2': 12,'E3': 13,'E4': 14}
-    render = mergeSameRowHtml(render_template('course.html',**locals()))
+    render = render_template('course.html',courseName = '課表',periodMapping=periodMapping,scheduleData=getScheduleData())
+    render = mergeSameRowHtml(render)
     return render
 
             
